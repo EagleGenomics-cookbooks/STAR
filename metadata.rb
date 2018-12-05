70fa83ba9a0f7324d6c6ed6020cb53698a5849e2
Craig Mcanulla <craig.mcanulla@eaglegenomics.com>
2018-12-05 16:23:27 +0000

    Updated chef version

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ license 'Apache-2.0'
 description 'Installs/Configures star'
 long_description 'Installs/Configures star'
 version '0.1.0'
-chef_version '>= 14' if respond_to?(:chef_version)
+chef_version '>= 12' if respond_to?(:chef_version)
 
 depends 'apt'
 depends 'magic_shell', '= 1.0.1'
